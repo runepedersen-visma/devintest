7c9aadb2f6448f0500a233ad4981af4d0fa0d54b
Rune Pedersen <dev_rup@adm.datakraftverk.no>
2025-08-08 10:23:26 +0200

    first

---
+++ Devin/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Check if today is Friday
+DateTime today = DateTime.Now;
+if (today.DayOfWeek == DayOfWeek.Friday)
+{
+    Console.WriteLine("It's Friday!");
+}
